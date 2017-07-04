ef6cc41d99f6b08fccae64981e6050f801eb840b
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-04 14:21:38 -0400

    Configuring devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [:sessions], controllers: {
+        sessions: 'users/sessions'
+      }
   resources :optatives
   root 'welcome#index'
 
