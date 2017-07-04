ac9de0c5734ae5cf058a7fb427e610663cac14ed
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-04 14:26:58 -0400

    Configuring cancancan and rolify

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.delete_all
+User.create username: 'admin', email: 'admin@host.com', password: 'administrator'
+User.first.add_role :admin 
