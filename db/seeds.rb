dcdd25c0014a2598af04e89e356f194226840407
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-05 15:42:09 -0400

    Going for showing optatives

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.create username: 'admin', email: 'admin@host.com', password: 'administrator
 User.first.add_role :admin 
 
 Optative.delete_all
-Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "computacion", enrollment: 30
+Optative.create title: 'Ruby on Rails', description: 'large description here...', professor: "Pedro Quintero", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "computacion", enrollment: 30
 Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "computacion", enrollment: 30
 Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "computacion", enrollment: 30
 Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "computacion", enrollment: 30
