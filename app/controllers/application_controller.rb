2d118bb1d1f1d89bfbcdca5f62ec7731f1c1738f
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-05 09:42:45 -0400

    Finished the alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do
-  	redirect_to root_path, notice: "Lo siento, permisos insuficientes para realizar esta acción. Si piensa que esto es un error, consulte al administrador de la aplicación."
+  	redirect_to root_path, alert: "Lo siento, permisos insuficientes para realizar esta acción. Si piensa que esto es un error, consulte al administrador de la aplicación."
   end
 
 end
