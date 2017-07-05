bebaad557293ebb92b672ea36126bfa3ad0ba852
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-05 12:44:08 -0400

    Some improves in the admin interface

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,23 @@
 User.delete_all
 User.create username: 'admin', email: 'admin@host.com', password: 'administrator'
 User.first.add_role :admin 
+
+Optative.delete_all
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
+Optative.create title: 'optativo', description: 'large description here...', professor: "Eric Nordelo", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "Computacion", enrollment: 30
