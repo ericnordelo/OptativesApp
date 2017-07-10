dfcac96e8c534281bdff666ee725d2ab7632f335
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-10 11:59:21 -0400

    Working remote authentication

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,9 +8,30 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+      client = Savon.client(
+      wsdl: "https://login.uh.cu/WebServices/CuoteService.asmx?WSDL",
+      endpoint: "https://login.uh.cu/WebServices/CuoteService.asmx",
+      ssl_verify_mode: :none
+      )
+
+      # [:buscar_empleados, :buscar_empleado, :buscar_por_assets, :buscar_estudiantes, :buscar_estudiante, :buscar_estudiantes_sigenu, :buscar_empleados_ext, :buscar_usuarios_dominio, :buscar_externos, :buscar_usuarios_por_login, :get_external_users, :get_internal_users, :get_student_users] 
+
+      # @response = client.call :get_student_users, message: { email: 'params', fullName: "", faculty: "", career: "", year: "", group: "", gender: "", hasInternet: "", hasChat: "", hasEmail: ""}
+
+
+      # p client.operations
+      if (User.where email: params.require(:user).permit(:email)[:email]).count != 0
+      else 
+        @response = client.call :estudiante_autentificarse, message: { name: params[:user][:email], password: params.require(:user).permit(:password)[:password]}
+        if ! @response.body[:estudiante_autentificarse_response][:estudiante_autentificarse_result].nil?
+          User.create username: params[:user][:email].split('@')[0], email: params[:user][:email], password: params.require(:user).permit(:password)[:password]
+          User.last.add_role :student
+        end
+      end
+
+      super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,7 @@
 
 User.delete_all
 User.create username: 'admin', email: 'admin@host.com', password: 'administrator'
-User.create username: 'ericng', email: 'eric.nordelo39@gmail.com', password: 'administrator'
 User.first.add_role :admin 
-User.last.add_role :student 
 
 Optative.delete_all
 Optative.create title: 'Ruby on Rails', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, ', professor: "Pedro Quintero", duration: 64, schedule: "9:00 a 1:00", year: 3, program: "computacion", enrollment: 30
