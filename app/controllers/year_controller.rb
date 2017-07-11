4e6644606f4aff5b11b11bd7dd5023b950125d16
ericng <e.nordelo@lab.matcom.uh.cu>
2017-07-11 06:29:15 -0400

    Finished already

--- app/controllers/year_controller.rb
+++ app/controllers/year_controller.rb
@@ -2,7 +2,38 @@ class YearController < ApplicationController
   load_and_authorize_resource
 
   def index
-  	
+  	if params[:año] && params[:carrera] && params[:semestre]
+  		@years = Year.where value: params[:año], program: params[:carrera], semester: params[:semestre]
+  		@msg = 'mostrando filtrados (por profesor, año y carrera)'
+  	elsif
+  		params[:año] && params[:carrera]
+  		@years = Year.where value: params[:año], program: params[:carrera]
+  		@msg = 'mostrando filtrados (por profesor y carrera)'
+  	elsif
+  		params[:semestre] && params[:carrera]
+  		@years = Year.where semester: params[:semestre], program: params[:carrera]
+  		@msg = 'mostrando filtrados (por año y carrera)'
+  	elsif
+  		params[:año] && params[:semestre]
+  		@years = Year.where value: params[:año], semester: params[:semestre]
+  		@msg = 'mostrando filtrados (por profesor y año)'
+  	elsif
+  		params[:año]
+  		@years = Year.where value: params[:año]
+  		@msg = 'mostrando filtrados (por profesor)'
+  	elsif
+  		params[:semestre]
+  		@years = Year.where semester: params[:semestre]
+  		@msg = 'mostrando filtrados (por año)'
+  	elsif
+  		params[:carrera]
+  		@years = Year.where program: params[:carrera]
+  		@msg = 'mostrando filtrados (por carrera)'
+  	else
+  		@years = Year.all
+  		@msg = 'mostrando todos'
+  	end
+    
   end
 
   def edit
